657f7a82faea3570d4d083e52deec4ca572d3150
pmtoam <pmtoam@qq.com>
2014-07-25 12:54:20 +0800

    save code.

--- EnterpriseAddressList/src/com/dooioo/eal/util/Logger.java
+++ EnterpriseAddressList/src/com/dooioo/eal/util/Logger.java
@@ -4,9 +4,14 @@ import android.util.Log;
 
 public class Logger
 {
+	/**
+	 * 
+	 * @param tag
+	 * @param msg
+	 */
 	public static void e(String tag, String msg)
 	{
-		Log.e(tag, "#####" + msg);
+		Log.e(tag, "#####---> " + msg);
 	}
 
 }
